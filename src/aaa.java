49a6fbf7e5007f5f6e549686f802389a1121b991
Santiago Gomez <gomezsanti69@gmail.com>
2022-10-22 13:25:00 -0500

    Ejercicio1

---
+++ src/aaa.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class aaa {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int f = 1;
+        System.out.println("¿Cuantos numeros primos quieres imprimir?: ");
+        int contadorPrimos = sc.nextInt();
+        while (f<= contadorPrimos) {
+        
+        for (int i = 1; f<= contadorPrimos; i++) {
+            int contador=0;
+            for (int j = 1; j <= i; j++) {
+                
+                int operacion = i%j;
+                if(operacion == 0){
+                    contador++;
+                }
+                
+            }
+            if(contador<=2){ 
+                System.out.println(i);
+                f++;
+            }
+        }
+        
+    }
+    }
+}
+
+
